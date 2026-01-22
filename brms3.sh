8d9d4bf82f043e144e0421ea1aca2661196abeb6
MikeMurphyIBM <michaelmurphy79@gmail.com>
2026-01-22 14:50:39 -0500

    Update job version in backup operations script

--- brms3.sh
+++ brms3.sh
@@ -26,7 +26,7 @@ set -eu
 ################################################################################
 echo ""
 echo "========================================================================"
-echo " JOB 3: BRMS BACKUP OPERATIONS v66"
+echo " JOB 3: BRMS BACKUP OPERATIONS v67"
 echo " Purpose: Execute cloud backups and synchronize BRMS history"
 echo "========================================================================"
 echo ""
